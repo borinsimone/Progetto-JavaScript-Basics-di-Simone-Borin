99f948e7d3dec45a005357b2dd1f57daedea9e6e
borinsimone <Borin.simone1997@gmail.com>
2022-01-18 11:11:11 +0100

    a

---
+++ js/script.js
@@ -0,0 +1,71 @@
+let part = document.getElementById("part");
+let toggler = document.getElementById("toggler");
+let hero = document.getElementById("hero");
+let content = document.getElementById("content");
+let info = document.getElementById("info");
+let plusElement = document.createElement("div");
+let minusElement = document.createElement("div");
+let num = document.createElement("p");
+let resetCounter = document.createElement("div");
+let i = 0;
+// CREATE CONTENT
+function createContent() {
+  plusElement.classList.add("plusClass");
+  minusElement.classList.add("minusClass");
+  num.classList.add("numClass");
+  resetCounter.classList.add("resetCounter");
+  content.classList.add("contentFadeIn");
+
+  hero.appendChild(resetCounter);
+  content.appendChild(minusElement);
+  content.appendChild(num);
+  num.innerHTML = i;
+
+  content.appendChild(plusElement);
+}
+
+toggler.addEventListener("click", function () {
+  console.log("ciao");
+  toggler.classList.add("bounce");
+  partLeft.classList.add("partFadeVertical");
+  partRight.classList.add("partFadeVertical");
+  partTop.classList.add("partFadeHorizontal");
+  partBottom.classList.add("partFadeHorizontal");
+  createContent();
+});
+
+// COUNTER & FANCY STUFF
+plusElement.addEventListener("click", function () {
+  console.log("ciao");
+  i++;
+  num.innerHTML = i;
+  num.classList.remove("animation");
+  resetFade();
+
+});
+minusElement.addEventListener("click", function () {
+  i--;
+  console.log(i);
+  num.innerHTML = i;
+  resetFade();
+  num.classList.remove("animation");
+  
+});
+
+resetCounter.addEventListener("click", function () {
+  if (i != 0) {
+    num.classList.add("animation");
+    i = 0;
+    resetFade();
+    num.innerHTML = i;
+    console.log(i);
+  }
+});
+
+function resetFade() {
+  if (i != 0) {
+    resetCounter.classList.add("infoVisible");
+  } else {
+    resetCounter.classList.remove("infoVisible");
+  }
+}
